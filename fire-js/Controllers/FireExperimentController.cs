40b653b1dfb72ed9f460ac00f49cdba50fc590aa
dbl4k <github@dbl4k.net>
2017-02-12 19:30:10 +0000

    Added FireExperiment model, FireExperimentController + Views

---
+++ fire-js/Controllers/FireExperimentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fire_js.Controllers
+{
+    public class FireExperimentController : Controller
+    {
+        // GET: FireExperiment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FireExperiment/Details
+        public ActionResult Details(int id)
+        {
+            return View(new Models.FireExperiment() { id = id });
+        }
+    }
+}
---
+++ fire-js/Models/FireExperiment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fire_js.Models
+{
+    public class FireExperiment
+    {
+        public int id = 0;
+        public String scriptname = "~/Scripts/...";
+
+    }
+}
