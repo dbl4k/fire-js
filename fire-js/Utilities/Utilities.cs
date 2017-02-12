8973589d98a88520c88f376a5b0800af65b9c6af
dbl4k <github@dbl4k.net>
2017-02-12 19:46:58 +0000

    Add Utilities class with mock data

---
+++ fire-js/Utilities/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fire_js
+{
+    public class Utilities
+    {
+        public static List<Models.FireExperiment> GetFireExperimentList()
+        {
+            // This is just a mock at this stage.
+            List<Models.FireExperiment> fireExperimentList = new List<Models.FireExperiment>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Models.FireExperiment fireExperiment = new Models.FireExperiment();
+                fireExperiment.id = i;
+                fireExperimentList.Add(fireExperiment);
+            }
+
+            return fireExperimentList;
+        }
+
+
+    }
+}
