f859ab5bf47bccb912a284dc3673264465802e0a
dbl4k <github@dbl4k.net>
2017-02-12 19:47:27 +0000

    Implement basic routing with mock data

--- fire-js/Controllers/FireExperimentController.cs
+++ fire-js/Controllers/FireExperimentController.cs
@@ -8,15 +8,24 @@ namespace fire_js.Controllers
 {
     public class FireExperimentController : Controller
     {
+
+        List<Models.FireExperiment> m_fireExperimentList = Utilities.GetFireExperimentList();
+
         // GET: FireExperiment
         public ActionResult Index()
         {
-            return View();
+            return View(m_fireExperimentList);
         }
 
         // GET: FireExperiment/Details
         public ActionResult Details(int id)
         {
+            // TODO will need to firm up existence before getting singular result, exception will occur.
+            Models.FireExperiment result = 
+                m_fireExperimentList.Where(
+                fireExperiment => fireExperiment.id.Equals(id)
+                ).FirstOrDefault();
+
             return View(new Models.FireExperiment() { id = id });
         }
     }
